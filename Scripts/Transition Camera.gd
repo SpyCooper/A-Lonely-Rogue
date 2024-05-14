a826cf7b597a94add0f92a05f3a65a268f7af5f5
SpyCooper <jm578220@ohio.edu>
2024-05-14 15:08:30 -0400

    HUD now goes to the same room as the camera

--- Scripts/Transition Camera.gd
+++ Scripts/Transition Camera.gd
@@ -1,6 +1,12 @@
 extends Camera2D
 
+@onready var hud = %HUD
+var hud_start_position : Vector2
+
 func _ready() -> void:
+	hud_start_position = hud.position
+	
 	Events.room_entered.connect(func(room):
 		global_position = room.global_position
+		hud.position = global_position + hud_start_position
 	)
