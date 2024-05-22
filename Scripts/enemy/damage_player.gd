b2f4157f9da3b611461cf9b013849f8c0920ce76
SpyCooper <jm578220@ohio.edu>
2024-05-22 12:51:41 -0400

    finished gelatinous cube fight combat

--- Scripts/enemy/damage_player.gd
+++ Scripts/enemy/damage_player.gd
@@ -1,8 +1,25 @@
 extends Area2D
 
+var player_in_damage = false
+@onready var timer = $Timer
+var player
+
 # collider is set to check layer 2 (which currently has the player)
 # when the player enters the collider, damage the player
 func _on_body_entered(body):
 	# if the entered body has the function damage_player(), it will run it
 	if body is Player:
 		body.player_take_damage()
+		player_in_damage = true
+		player = body
+		timer.start()
+
+func _on_body_exited(_body):
+	player_in_damage = false
+	timer.stop()
+
+
+func _on_timer_timeout():
+	if player_in_damage == true:
+		player.player_take_damage()
+		timer.start()
--- Scripts/enemy/gelatinous_cube.gd
+++ Scripts/enemy/gelatinous_cube.gd
@@ -1,16 +1,30 @@
 extends Enemy
 
+enum spawn_location
+{
+	top,
+	bottom,
+	left,
+	right
+}
+
+
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var damage_player = $DamagePlayer
 @onready var attack_timer = $Attack_timer
 @onready var death_timer = $Death_timer
 @onready var spawn_timer = $Spawn_timer
+@onready var spawn_freeze_timer = $Spawn_freeze_timer
 
 @onready var spawner_top = $Spawner_top
 @onready var spawner_bottom = $Spawner_bottom
 @onready var spawner_left = $Spawner_left
 @onready var spawner_right = $Spawner_right
 const GREEN_SLIME = preload("res://Scenes/enemies/green_slime.tscn")
+@onready var animated_top = $animated_top
+@onready var animated_bottom = $animated_bottom
+@onready var animated_left = $animated_left
+@onready var animated_right = $animated_right
 
 var random_number_generator = RandomNumberGenerator.new()
 
@@ -20,12 +34,19 @@ var plaing_hit_animation = false
 var can_attack = false
 var dying = false
 var spawning = false
+var can_move = true
+
+var slime_to_be_spawned = []
 
 func _ready():
 	speed = 0.25
 	health = 30
 	sleep()
 	player = Events.player
+	animated_bottom.hide()
+	animated_left.hide()
+	animated_right.hide()
+	animated_top.hide()
 	Events.room_entered.connect(func(room):
 		if room == get_parent():
 			wake_up()
@@ -42,7 +63,7 @@ func _physics_process(_delta):
 			spawn_slimes()
 		
 		# follows the player
-		if player:
+		if player && can_move:
 			player_position = player.position
 			target_position = (player_position - global_position).normalized()
 			current_direction = get_look_direction(target_position)
@@ -104,34 +125,39 @@ func get_animated_sprite():
 	return animated_sprite
 
 func spawn_slimes():
-	var random_spawns = random_number_generator.randi_range(0, 3)
+	var random_spawns = random_number_generator.randi_range(1, 3)
 	var top_used = false;
 	var bottom_used = false;
 	var right_used = false;
 	var left_used = false;
 	for spawn in random_spawns:
-		var green_slime = GREEN_SLIME.instantiate()
-		get_parent().add_child(green_slime)
-		green_slime.spawned_in_room()
-		
 		var random_pos = random_number_generator.randi_range(0, 3)
 		if random_pos == 0 && top_used == false:
-			green_slime.global_position = spawner_top.global_position
+			slime_to_be_spawned += [spawn_location.top]
+			animated_top.show()
+			animated_top.play("default")
 			top_used = true
 		elif random_pos == 1 && right_used == false:
-			green_slime.global_position = spawner_right.global_position
+			slime_to_be_spawned += [spawn_location.right]
+			animated_right.show()
+			animated_right.play("default")
 			right_used = true
 		elif random_pos == 2 && left_used == false:
-			green_slime.global_position = spawner_left.global_position
+			slime_to_be_spawned += [spawn_location.left]
+			animated_left.show()
+			animated_left.play("default")
 			left_used = true
 		elif random_pos == 3 && bottom_used == false:
-			green_slime.global_position = spawner_bottom.global_position
+			slime_to_be_spawned += [spawn_location.bottom]
+			animated_bottom.show()
+			animated_bottom.play("default")
 			bottom_used = true
-		else:
-			green_slime.despawn()
 	
+	spawn_freeze_timer.start()
 	attack_timer.start()
 	can_attack = false
+	can_move = false
+	animated_sprite.play("spawn_slime")
 
 func _on_attack_timer_timeout():
 	can_attack = true
@@ -152,3 +178,34 @@ func spawn_in():
 
 func _on_spawn_timer_timeout():
 	spawning = false
+
+func _on_spawn_freeze_timer_timeout():
+	can_move = true
+	for spawn in slime_to_be_spawned:
+		var green_slime = GREEN_SLIME.instantiate()
+		get_parent().add_child(green_slime)
+		green_slime.spawned_in_room()
+		var vect = Vector2(1,1)
+		if spawn == spawn_location.top:
+			green_slime.global_position = spawner_top.global_position
+			vect = Vector2(0.0,1.0)
+		elif spawn == spawn_location.right:
+			green_slime.global_position = spawner_right.global_position
+			vect = Vector2(1.0,0.0)
+		elif spawn == spawn_location.left:
+			green_slime.global_position = spawner_left.global_position
+			vect = Vector2(-1.0,0.0)
+		elif spawn == spawn_location.bottom:
+			green_slime.global_position = spawner_bottom.global_position
+			vect = Vector2(0.0,1.0)
+		
+		var collision = green_slime.move_and_collide(vect)
+		if collision != null:
+			green_slime.despawn()
+	
+	animated_bottom.hide()
+	animated_left.hide()
+	animated_right.hide()
+	animated_top.hide()
+	
+	slime_to_be_spawned = []
