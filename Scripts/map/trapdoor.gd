e2ec9fd0432cf91cfa3c9ab65218252f9be2a91c
SpyCooper <jm578220@ohio.edu>
2024-06-07 15:10:42 -0400

    created maps for floors 2 and 3

--- Scripts/map/trapdoor.gd
+++ Scripts/map/trapdoor.gd
@@ -12,3 +12,5 @@ func _on_area_2d_body_entered(body):
 		## NOTE: this check uses the node name
 		if get_tree().current_scene.name == "Floor1":
 			Events.floor_changed.emit("res://Scenes/Dungeon_floors/dungeon_floor_2.tscn")
+		if get_tree().current_scene.name == "Floor2":
+			Events.floor_changed.emit("res://Scenes/Dungeon_floors/dungeon_floor_3.tscn")
